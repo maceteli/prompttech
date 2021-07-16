42beb3f5bc8c39960270b2bb62ee671f8ea43db8
maceteli <maceteligolden@gmail.com>
2021-07-16 19:55:27 +0100

    fixes for mobile view postitioning and navigation

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,7 @@
 //Navbar scroll effect
 window.onscroll = function() {scrollFunction()};
+//to show navbar on mobile view
+var btn2 = document.getElementById('menu-btn');
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -13,4 +15,17 @@ function scrollFunction() {
   } else {
     document.getElementById("content").classList.remove("hero-fix");
   }
+}
+
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'flex') {
+    e.style.display = 'none';
+  }
+  else {
+    e.style.display = 'flex';
+  }
+  console.log('clicked');
+  return;
 }
